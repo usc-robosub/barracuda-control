732930dc2aa238aecb73ecdb497cc57878837b56
Alejandro Arteaga <ajarteag@usc.edu>
2026-03-01 12:20:00 -0800

    fixed entrypoint comment error

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,8 +1,3 @@
-# #!/usr/bin/bash
-# source /opt/ros/humble/setup.bash
-# source /opt/barracuda-control/dev_ws/install/setup.bash
-# ros2 launch barracuda_control joystick_wrench_controller.launch.py
-
 #!/bin/bash
 set -e
 
