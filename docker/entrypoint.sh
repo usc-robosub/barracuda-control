3e4b0a5f3db32167447b39efd6d3c51af4cb7ac1
Alejandro Arteaga <ajarteag@usc.edu>
2026-03-01 12:14:59 -0800

    restructed directory to pkg again and rewrote docker files

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,5 +1,23 @@
-#!/usr/bin/bash
-source /opt/ros/humble/setup.bash
-source /opt/barracuda-control/dev_ws/install/setup.bash
-ros2 launch barracuda_control joystick_wrench_controller.launch.py
-# ros2 launch foxglove_bridge foxglove_bridge_launch.xml 
+# #!/usr/bin/bash
+# source /opt/ros/humble/setup.bash
+# source /opt/barracuda-control/dev_ws/install/setup.bash
+# ros2 launch barracuda_control joystick_wrench_controller.launch.py
+
+#!/bin/bash
+set -e
+
+echo "Installing dependencies..."
+apt-get update
+
+rosdep install --from-paths src --ignore-src -y
+
+echo "Building ROS 2 workspace..."
+colcon build --symlink-install
+
+source install/setup.bash
+
+echo "=========================================="
+echo " Barracuda Control Workspace Ready! "
+echo "=========================================="
+
+exec "$@"
