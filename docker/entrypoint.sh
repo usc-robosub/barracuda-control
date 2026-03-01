edc382473cc25e933cf01f1fd24b192ac6d5539a
Alejandro Arteaga <ajarteag@usc.edu>
2026-03-01 12:46:32 -0800

    debugging container dependencies

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -7,9 +7,9 @@ apt-get update
 rosdep install --from-paths src --ignore-src -y
 
 echo "Building ROS 2 workspace..."
-colcon build --symlink-install
+# colcon build --symlink-install
 
-source install/setup.bash
+# source install/setup.bash
 
 echo "=========================================="
 echo " Barracuda Control Workspace Ready! "
