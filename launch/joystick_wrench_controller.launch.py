ed05a5f6d324eb941119e9388be1a12648974109
mihir <mihirsin@usc.edu>
2026-03-05 16:54:04 -0800

    removed foxglove bridge, thruster manager launch inclusions in launch file

--- launch/joystick_wrench_controller.launch.py
+++ launch/joystick_wrench_controller.launch.py
@@ -1,32 +1,5 @@
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
-from launch.substitutions import PathJoinSubstitution
 from launch_ros.actions import Node
-from launch_ros.substitutions import FindPackageShare
-
-thruster_manager_params = {
-    "tam.thrusters": [
-        "barracuda/thruster_0_joint",
-        "barracuda/thruster_1_joint",
-        "barracuda/thruster_2_joint",
-        "barracuda/thruster_3_joint",
-        "barracuda/thruster_4_joint",
-        "barracuda/thruster_5_joint",
-        "barracuda/thruster_6_joint",
-        "barracuda/thruster_7_joint",
-    ],
-    "tam.min_thrust": -4.0,
-    "tam.max_thrust": 4.0,
-    "control_frame": "barracuda/base_link",
-}
-# create thruster_manager node
-node_thruster_manager = Node(
-    package="thruster_manager",
-    namespace="barracuda",
-    executable="thruster_manager_node",
-    output="screen",
-    parameters=[thruster_manager_params],
-)
 
 node_joystick_to_wrench = Node(
     package="barracuda_control",
@@ -39,16 +12,6 @@ node_joystick_to_wrench = Node(
 def generate_launch_description():
     return LaunchDescription(
         [
-            IncludeLaunchDescription(
-                PathJoinSubstitution(
-                    [
-                        FindPackageShare("foxglove_bridge"),
-                        "launch",
-                        "foxglove_bridge_launch.xml",
-                    ]
-                ),
-            ),
-            node_thruster_manager,
             node_joystick_to_wrench,
         ]
     )
