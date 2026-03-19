d3303aa2f1af03e69a2fb45e1bd0a57ee98d6b1d
mihir <mihirsin@usc.edu>
2026-03-18 22:01:41 -0700

    added barracuda_control.launch.py

---
+++ launch/barracuda_control.launch.py
@@ -0,0 +1,15 @@
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.substitutions import PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
+
+def generate_launch_description():
+    return LaunchDescription(
+        IncludeLaunchDescription(
+            FindPackageShare("barracuda_control"),
+            "launch",
+            "joystick_wrench_controller.launch.py"
+        )       
+    )
+
+    
